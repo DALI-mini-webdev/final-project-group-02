131ffa094fda5b5529a49e61ba8e84db57d26e26
camdenhao <camden.hao@gmail.com>
2021-02-23 23:55:57 -0500

    initial react app + input/firebase

--- group-2/src/components/HouseList.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from "react"; 
-
-function HouseList(){
-    const houses = ["TDX", "GDX", "Psi U", "Heorot", "AXA", "Beta", "Chi Gam", "Sig Nu", "Tri Kap", "Zete", "Phi Delt", "BG"]
-}
