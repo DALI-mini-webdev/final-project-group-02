84e32502ef1d862c5308ff5047c20bc42da055bb
gyur2hwng <gyuri.hwang.23@dartmouth.edu>
2021-02-25 01:52:42 +0900

    CSS additions on input and app

--- group-2/src/App.js
+++ group-2/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import firebase from './firebase/index';
 import Input from './components/Input'
+
 function App() {
   console.log(firebase);
   console.log(firebase.db);
   return (
     <div className="App">
-      <Input />
+      <div className="App-header">
+        <h3>To Enjoy Dartmouth Greek Life Better...</h3>
+        <h1>No Longer Freeze Outside <br></br>
+        Searching for Open Houses!</h1>
+      </div>
+      
+      <div className="App-body">
+        <Input />
+      </div>
+      
     </div>
   );
 }
