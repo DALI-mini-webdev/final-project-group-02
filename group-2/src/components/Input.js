42907e5d7c3249006c331d9d95cdf2aec7f6c6a2
camdenhao <camden.hao@gmail.com>
2021-02-28 16:42:22 -0500

    added date display

--- group-2/src/components/Input.js
+++ group-2/src/components/Input.js
@@ -4,13 +4,12 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 import './input.css'
 
-
 class Input extends Component{
     constructor(props){
         super(props);
         this.state = {
             name: '',
-            dates: []
+            dates: new Set()
         }
     }
 
@@ -18,44 +17,70 @@ class Input extends Component{
         this.setState({name: event.target.value})
     }
 
-    newDatesFunction = (event) => {
-        this.setState({dates: event.target.value.split(",")})
+    onClickDay = (value) => {
+        var temp = String(value)
+        var list = this.state.dates  
+        if(list.has(temp)){
+            list.delete(temp) 
+        }
+        else{
+            list.add(temp)
+        }
+        this.setState({dates: list})
     }
-
     saveNewInfo = () => {
-        firebase.firestore().collection('houses').doc(this.state.name).set({
-            name: this.state.name,
-        }, { merge: true }).catch((e) => {
-            console.log(e);
-        });
-        for(var i = 0; i < this.state.dates.length; i++){
+        if(this.state.name != ''){
             firebase.firestore().collection('houses').doc(this.state.name).set({
-                dates: firebase.firestore.FieldValue.arrayUnion(this.state.dates[i])
+                name: this.state.name,
             }, { merge: true }).catch((e) => {
                 console.log(e);
             });
+            for(let item of this.state.dates){
+                firebase.firestore().collection('houses').doc(this.state.name).set({
+                    dates: firebase.firestore.FieldValue.arrayUnion(item)
+                }, { merge: true }).catch((e) => {
+                    console.log(e);
+                });
+            }
+            this.setState({
+                dates: new Set(),
+                name: ''
+                })
         }
+        
     }
     render(){
+        var list = Array.from(this.state.dates)
+        var dateList = list.map((date) => {
+            var str=date
+            str=str.substring(0,15)
+            return(
+                <p className='date'>{str}</p>
+            )}
+        )
         return(
             <div className='input'>
                 <h2>For Greek Houses:</h2>
                 <p>Enter the name of the House You Want to Add:</p>
                 <input type="text" value={this.state.name} onChange={this.newHouseFunction} />
-                <p>Enter the Dates When that House is Open:</p>
-                <input type="text" onChange={this.newDatesFunction} />
-
-                <br></br>
-                <br></br>
-                <button onClick={this.saveNewInfo}>Submit!</button>
                 
+                <br></br>
+                <br></br>
+
+                <p>Select Dates:</p>
                 <div className='student-section'>
-                    <h2>For Students:</h2>
                 </div>
-                <div className="calendar"> 
-                    <Calendar />
+                <div className='flex'>
+                    <div className="calendar"> 
+                        <Calendar onClickDay={this.onClickDay}/>
+                        <button onClick={this.saveNewInfo}>Submit!</button>
+                    </div>
+                    <div>
+                        <p>Selected Dates:</p>
+                        {dateList}
+                    </div>
                 </div>
-            
+                
             </div>
         )
     }
