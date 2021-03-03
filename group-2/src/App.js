e2d6dbbd348db60a23cee3516a38e57fff26b9db
marktao23 <63976152+marktao23@users.noreply.github.com>
2021-03-03 16:44:17 -0600

    Update App.js

--- group-2/src/App.js
+++ group-2/src/App.js
@@ -11,29 +11,106 @@ import {
 } from 'react-router-dom';
 
 var allHouses = ["Tri Kap", "Chi Gam", "TDX", "GDX", "Psi U", "AXA", "Zete", "Heoret", "SAE", "BG", "Phi Delt"]
-var areOpen = ["closed", "closed", "closed", "closed", "closed", "closed", "closed", "closed", "closed", "closed", "closed"]
+var housestring;
 
-function HouseList(houseNames) {
-  for (var i = 0; i < houseNames.length; i++) {
-    if (allHouses.includes(houseNames[i])) {
-      var temp = allHouses.indexOf(houseNames[i])
-      areOpen[temp] = "open"
-    }
-  }
-}
+const docRef = firebase.db.collection("dates").doc("March 1");
 
-var newArray = [];
-
-function one(docRef) {
-  docRef.get().then((doc) => {
-    let data = doc.data();
-    return data;
-  })
-};
 
 function App() {
-  console.log(firebase);
-  console.log(firebase.db);
+  
+    const [AXA, setAXA] = useState('closed');
+    const [TDX, setTDX] = useState('closed');
+    const [GDX, setGDX] = useState('closed');
+    const [ChiGam, setChiGam] = useState('closed');
+    const [TriKap, setTriKap] = useState('closed');
+    const [PsiU, setPsiU] = useState('closed');
+    const [Zete, setZete] = useState('closed');
+    const [Heoret, setHeoret] = useState('closed');
+    const [SAE, setSAE] = useState('closed');
+    const [BG, setBG] = useState('closed');
+    const [PhiDelt, setPhiDelt] = useState('closed');
+    const [SigNu, setSigNu] = useState('closed');
+    
+  docRef.get().then((doc) => {
+      let data = doc.data();
+      housestring = data.houses.split(", ")
+      console.log(housestring)
+
+      if (housestring.includes("AXA")) {
+        setAXA('open'); 
+      }
+      else {
+        setAXA('closed');
+      }
+      if (housestring.includes("TDX")) {
+        setTDX('open'); 
+      }
+      else {
+        setTDX('closed');
+      }
+      if (housestring.includes("GDX")) {
+        setGDX('open'); 
+      }
+      else {
+        setGDX('closed');
+      }
+      if (housestring.includes("Chi Gam")) {
+        setChiGam('open'); 
+      }
+      else {
+        setChiGam('closed');
+      }
+      if (housestring.includes("Tri Kap")) {
+        setTriKap('open'); 
+      }
+      else {
+        setTriKap('closed');
+      }
+      if (housestring.includes("Psi U")) {
+        setPsiU('open'); 
+      }
+      else {
+        setPsiU('closed');
+      }
+      if (housestring.includes("Zete")) {
+        setZete('open'); 
+      }
+      else {
+        setZete('closed');
+      }
+      if (housestring.includes("Heoret")) {
+        setHeoret('open'); 
+      }
+      else {
+        setHeoret('closed');
+      }
+      if (housestring.includes("SAE")) {
+        setSAE('open'); 
+      }
+      else {
+        setSAE('closed');
+      }
+      if (housestring.includes("BG")) {
+        setBG('open'); 
+      }
+      else {
+        setBG('closed');
+      }
+      if (housestring.includes("Phi Delt")) {
+        setPhiDelt('open'); 
+      }
+      else {
+        setPhiDelt('closed');
+      }
+      if (housestring.includes("Sig Nu")) {
+        setSigNu('open');
+      }
+      else {
+        setSigNu('closed');
+      }
+    }
+    );
+  
   return (
     <div className="App">
       <Router>
